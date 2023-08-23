9e6d08c8958ba050cdc4e6f6ee4ef8780274ce76
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2023-08-23 17:39:25 +0300

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,42 +7,45 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   }
 
-createContact = values => {
-    const targetContact = this.state.contacts
-      .map((cont) => cont.name.toLowerCase())
-      .includes(values.name.toLowerCase());
+  async componentDidMount() { 
+  const savedContacts = await localStorage.getItem("contacts");
+  const parsedContacts = JSON.parse(savedContacts);
+  this.setState({ contacts: parsedContacts });
+}
+
+createContact = async (values) => {
+  const targetContact = this.state.contacts
+    .map((cont) => cont.name.toLowerCase())
+    .includes(values.name.toLowerCase());
 
     if (targetContact) {
       alert(`${values.name} is already in contacts`);
     } else {
       values.id = nanoid();
-      this.setState(prevState => {
+      const savedContacts = await this.setState(prevState => {
         return {
           contacts: [...prevState.contacts, values],
         };
       });
   };
+  localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
 };
 
 changeFilter = searchValue => {
   this.setState({filter: `${searchValue.target.value}`});
 }
 
-handleDelete = contactId => {
-  this.setState(prevState => {
+handleDelete = async (contactId) => {
+  const savedContacts = await this.setState(prevState => {
     return {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     };
   });
+  localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
 };
 
   render() {
